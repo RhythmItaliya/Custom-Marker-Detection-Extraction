5f30a1412eb9c156d3878413ed409305de25b7ea
rhythmitaliya <ritaliya2003@gmail.com>
2026-05-10 20:44:39 +0530

    build: configure path aliasing and environment dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,9 @@ module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 80,
+  endOfLine: 'auto',
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
 };
